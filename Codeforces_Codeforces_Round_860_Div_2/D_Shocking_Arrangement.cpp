3ef462ea1a81a91ca55606e5106495260eb431e4
Angellopp <angellojamir@hotmail.com>
2024-03-01 10:40:16 -0500

    Some Problems

---
+++ Codeforces_Codeforces_Round_860_Div_2/D_Shocking_Arrangement.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define fast_io ios_base::sync_with_stdio(false); cin.tie (NULL)
+
+using namespace std;
+
+int main() {
+    fast_io;
+    int tt;
+    cin >> tt;
+    while (tt--) {
+        int n;
+        cin >> n;
+    }
+    return 0;
+}
